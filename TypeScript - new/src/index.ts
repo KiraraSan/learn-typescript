a0837504aade17a4a7b3e7ba2ad9312536c4ac46
Alexis Ngaing <ksankirara92@gmail.com>
2022-12-30 13:42:04 +0700

    initial commit

--- TypeScript - new/src/index.ts
+++ TypeScript - new/src/index.ts
@@ -19,11 +19,20 @@
 // let user: [number, string, boolean, number] = [1, 'Alex', true, 0];
 
 /* enums */
-const small = 1;
-const medium = 2;
-const large = 3;
+// const small = 1;
+// const medium = 2;
+// const large = 3;
 
 // PascalCase
-enum Size { Small = 1, Medium , Large };
-let mySize: Size = Size.Medium;
-console.log(mySize);
+// const enum Size { Small = 1, Medium , Large };
+// let mySize: Size = Size.Medium;
+// console.log(mySize);
+
+/* functions */
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+calculateTax(10_000);
