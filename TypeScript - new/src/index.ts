9da5725ef0d32afb8b88eb4b6e146722008dc55a
Alexis Ngaing <ksankirara92@gmail.com>
2022-12-30 13:48:26 +0700

    initial commit

--- TypeScript - new/src/index.ts
+++ TypeScript - new/src/index.ts
@@ -29,10 +29,23 @@
 // console.log(mySize);
 
 /* functions */
-function calculateTax(income: number, taxYear = 2022): number {
-    if (taxYear < 2022)
-        return income * 1.2;
-    return income * 1.3;
-}
+// function calculateTax(income: number, taxYear = 2022): number {
+//     if (taxYear < 2022)
+//         return income * 1.2;
+//     return income * 1.3;
+// }
 
-calculateTax(10_000);
+// calculateTax(10_000);
+
+/* objects */
+let employee: {
+    readonly id: number,
+    name: string
+    retire: (date: Date) => void
+} = { 
+    id: 1, 
+    name: 'Alex'
+    retire: (date: Date) => {
+        console.log(date);
+    } 
+};
